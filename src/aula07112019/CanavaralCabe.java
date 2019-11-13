de956c00374782ffe7cb9e820405ac65630fc528
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-11-13 00:15:21 -0300

    Excecoes

--- src/aula07112019/CanavaralCabe.java
+++ src/aula07112019/CanavaralCabe.java
@@ -1,14 +1,21 @@
 package aula07112019;
 
+import java.util.concurrent.TimeoutException;
+
 public class CanavaralCabe {
 	public static void main(String[] args) {
 		Rocket poo1 = new Rocket();
+		
 		try {
-			poo1.launch(1220);
-			System.out.print("It seems okay...");
+			System.out.println("Entrou no try");
+			poo1.launch(1900);
+			System.out.println("It seems okay...");
 			
-		} catch(HighTemperatureException e) {
+		} catch(HighTemperatureException | TimeoutException e) {
 			System.out.println(e.getMessage());
+			
+		} finally {
+			System.out.println("Entrou no finally");
 		}
 
 	}
--- src/aula07112019/Rocket.java
+++ src/aula07112019/Rocket.java
@@ -1,12 +1,33 @@
 package aula07112019;
 
+import java.util.concurrent.TimeoutException;
+
 public class Rocket {
 	
-	public void launch(double temperature) throws HighTemperatureException {
+	public void launch(double temperature)
+			throws HighTemperatureException, TimeoutException {
+		
+		try {
+			System.out.println("alocando recurso...");
+			prepare(temperature / 3);
+		} finally {
+			System.out.println("desalocando recurso...");
+		}
+		
 		if (temperature > 1000) {
 			throw new HighTemperatureException(temperature);
 		}
 		
 		System.out.print("Okay houston.");
+	}
+	
+	public void prepare(double time) throws TimeoutException {
+		
+		System.out.println("Preparing launch");
+		
+		if (time > 300) {
+			throw new TimeoutException();
+		}
+		
 	}
 }
